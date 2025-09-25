2a2eafbd3bc627ece89da9649c5804a87602845a
zer0reaction <boltgopnik@gmail.com>
2025-09-25 12:50:39 +0300

    Implement tours template rendering

--- web_Hello_var_26/firstapp_var_26/views.py
+++ web_Hello_var_26/firstapp_var_26/views.py
@@ -8,4 +8,31 @@ def navigator(request):
     return render(request, "navigator.html")
 
 def tours(request):
-    return render(request, "tours.html")
+    tours = [
+        {
+            "name": "Золотое кольцо России",
+            "type": "Экскурсионный",
+            "season": "Лето",
+            "duration": "5 дней, 4 ночи",
+        },
+        {
+            "name": "Осень в Карелии",
+            "type": "Экскурсионный",
+            "season": "Осень",
+            "duration": "3 дня, 2 ночи",
+        },
+        {
+            "name": "Знакомство с Байкалом. Осень",
+            "type": "Экскурсионный",
+            "season": "Осень",
+            "duration": "5 дней, 4 ночи",
+        },
+        {
+            "name": "Восхождение на Эльбрус",
+            "type": "Восхождение",
+            "season": "Осень",
+            "duration": "8 дней, 7 ночей",
+        },
+    ]
+
+    return render(request, "tours.html", context={"tours": tours})
