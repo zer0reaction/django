ba0bd0d8d85e7adf5be6f1a8a34084a2af574cf6
zer0reaction <boltgopnik@gmail.com>
2025-09-25 14:41:07 +0300

    Implement loading table data from json

--- web_Hello_var_26/firstapp_var_26/views.py
+++ web_Hello_var_26/firstapp_var_26/views.py
@@ -1,17 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+import json
+
+tours_data = None
+agencies_data = None
+
+with open('firstapp_var_26/table/tours.json') as file:
+    tours_data = json.load(file)
+
+with open('firstapp_var_26/table/agencies.json') as file:
+    agencies_data = json.load(file)
+
 def index(request):
     return render(request, "index.html")
 
 def navigator(request):
     return render(request, "navigator.html")
 
-def tours(request, tours):
-    return render(request, "tours.html", context={"tours": tours})
+def tours(request):
+    return render(request, "tours.html", context={"tours": tours_data})
 
-def agencies(request, agencies):
-    return render(request, "agencies.html", context={"agencies": agencies})
+def agencies(request):
+    return render(request, "agencies.html", context={"agencies": agencies_data})
 
 def preparation(request):
     return render(request, "preparation.html")
